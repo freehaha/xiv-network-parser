2da4f84a1835f40e01a098f2e9effaff1a873bda
Freehaha <freehaha@gmail.com>
2020-01-17 23:46:54 +1100

    remove unused parts

--- src/ffxiv.rs
+++ src/ffxiv.rs
@@ -11,16 +11,6 @@ pub enum XivPacketType {
     Unknown,
     Lobby,
     Ignore,
-    LeaveZone = 0x0274,
-    Action = 0x01c6,
-    Action8 = 0x2c03,
-    Action16 = 0x02be,
-    Action24 = 0x0076,
-    Tick = 0x00bc,
-    Status = 0x025e,
-    Casting = 0x033e,
-    Dialog = 0x03ad,
-    StatusEffect = 0x023c,
 }
 
 impl XivPacketType {
@@ -37,28 +27,11 @@ impl XivPacketType {
             return XivPacketType::Lobby;
         }
         rdr.seek(SeekFrom::Start(18)).unwrap();
-        let ptype = rdr.read_u32::<LittleEndian>().unwrap();
-        return match ptype {
-            0x0274 => XivPacketType::LeaveZone,
-            0x01c6 => XivPacketType::Action,
-            0x02c3 => XivPacketType::Action8,
-            0x02be => XivPacketType::Action16,
-            0x0076 => XivPacketType::Action24,
-            0x00bc => XivPacketType::Tick,
-            0x025e => XivPacketType::Status,
-            0x033e => XivPacketType::Casting,
-            0x03ad => XivPacketType::Dialog,
-            0x023c => XivPacketType::StatusEffect,
-            _ => XivPacketType::Unknown,
-        };
+        return XivPacketType::Unknown;
     }
 }
 
 impl XivPacket {
-    // pub fn into_inner(self) -> Vec<u8> {
-    //     return self.packet;
-    // }
-    //
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut out = Vec::new();
         out.write_u64::<LittleEndian>(self.timestamp).unwrap();
