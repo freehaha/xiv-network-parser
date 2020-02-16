a3d347f5a89ce9c83d14f1c7dce532a70d01ce98
Freehaha <freehaha@gmail.com>
2020-02-16 14:43:41 +1100

    handle non-ether packet for ipip tunnel

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() {
         }
     }
     if let None = device {
-        println!("could not find device {}", DEVICE);
+        println!("could not find device {}", interface);
         return;
     }
 
@@ -88,84 +88,98 @@ fn main() {
             skip = 5;
             next_seq = 0;
         }
+
+        // parsing closure
+        let mut parse_tcp = |tcp: TcpPacket| {
+            if port > 0 && tcp.get_destination() != port {
+                return;
+            }
+            // println!(
+            //     "{}:{} => {}:{}",
+            //     v4p.get_source(),
+            //     tcp.get_source(),
+            //     v4p.get_destination(),
+            //     tcp.get_destination()
+            // );
+            let payload = tcp.payload().to_vec();
+            // println!("payload len: {}", payload.len());
+            // println!("seq: {}", tcp.get_sequence());
+            if port == 0 {
+                if skip > 0 {
+                    skip -= 1;
+                    return;
+                }
+                if payload.len() > 4 && XIV_MAGIC == payload[0..4] {
+                    println!("got game packet! using port {}", tcp.get_destination());
+                    port = tcp.get_destination();
+                    next_seq = tcp.get_sequence();
+                    skip = -1;
+                } else {
+                    return;
+                }
+            }
+
+            if next_seq > 0 && tcp.get_sequence() > next_seq {
+                println!(
+                    "expecting seq {}, got {}, caching...",
+                    next_seq,
+                    tcp.get_sequence()
+                );
+                future_packets.insert(tcp.get_sequence(), payload);
+                if future_packets.len() > 10 {
+                    println!("too many packets cached, restarting..");
+                    let mut min_seq = 0xffffffff;
+                    for &sseq in future_packets.keys() {
+                        if let Some(packet) = future_packets.get(&sseq) {
+                            if packet.len() >= 4 && XIV_MAGIC != packet[0..4] {
+                                continue;
+                            }
+                            if sseq < min_seq {
+                                min_seq = sseq;
+                            }
+                        }
+                    }
+                    if min_seq < 0xffffffff {
+                        println!("continuing from {}", min_seq);
+                        let payload = future_packets.get(&min_seq).unwrap();
+                        next_seq = min_seq + packet.len() as u32;
+                        parser = Parser::new(&socket);
+                        parser.parse_packet(payload);
+                        return;
+                    }
+                    future_packets.clear();
+                    port = 0;
+                    return;
+                }
+            } else {
+                // println!("expecting seq {}, got {}", next_seq, tcp.get_sequence());
+                parser.parse_packet(&payload);
+                // process_packet(&mut buffer, &payload);
+                next_seq += payload.len() as u32;
+                while future_packets.contains_key(&next_seq) {
+                    let payload = future_packets.remove(&next_seq).unwrap();
+                    println!("found future packet {}", next_seq);
+                    parser.parse_packet(&payload);
+                    next_seq += payload.len() as u32;
+                }
+            }
+            next_seq = next_seq & 0xffffffff;
+        };
+
         let eth = EthernetPacket::new(packet.data).unwrap();
         if let EtherTypes::Ipv4 = eth.get_ethertype() {
             let v4p = Ipv4Packet::new(eth.payload()).unwrap();
             if let IpNextHeaderProtocols::Tcp = v4p.get_next_level_protocol() {
                 let tcp = TcpPacket::new(v4p.payload()).unwrap();
-                if port > 0 && tcp.get_destination() != port {
-                    continue;
-                }
-                // println!(
-                //     "{}:{} => {}:{}",
-                //     v4p.get_source(),
-                //     tcp.get_source(),
-                //     v4p.get_destination(),
-                //     tcp.get_destination()
-                // );
-                let payload = tcp.payload().to_vec();
-                // println!("payload len: {}", payload.len());
-                // println!("seq: {}", tcp.get_sequence());
-                if port == 0 {
-                    if skip > 0 {
-                        skip -= 1;
-                        continue;
-                    }
-                    if payload.len() > 4 && XIV_MAGIC == payload[0..4] {
-                        println!("got game packet! using port {}", tcp.get_destination());
-                        port = tcp.get_destination();
-                        next_seq = tcp.get_sequence();
-                        skip = -1;
-                    } else {
-                        continue;
-                    }
-                }
-
-                if next_seq > 0 && tcp.get_sequence() > next_seq {
-                    println!(
-                        "expecting seq {}, got {}, caching...",
-                        next_seq,
-                        tcp.get_sequence()
-                    );
-                    future_packets.insert(tcp.get_sequence(), payload);
-                    if future_packets.len() > 10 {
-                        println!("too many packets cached, restarting..");
-                        let mut min_seq = 0xffffffff;
-                        for &sseq in future_packets.keys() {
-                            if let Some(packet) = future_packets.get(&sseq) {
-                                if packet.len() >= 4 && XIV_MAGIC != packet[0..4] {
-                                    continue;
-                                }
-                                if sseq < min_seq {
-                                    min_seq = sseq;
-                                }
-                            }
-                        }
-                        if min_seq < 0xffffffff {
-                            println!("continuing from {}", min_seq);
-                            let payload = future_packets.get(&min_seq).unwrap();
-                            next_seq = min_seq + packet.len() as u32;
-                            parser = Parser::new(&socket);
-                            parser.parse_packet(payload);
-                            continue;
-                        }
-                        future_packets.clear();
-                        port = 0;
-                        continue;
-                    }
-                } else {
-                    // println!("expecting seq {}, got {}", next_seq, tcp.get_sequence());
-                    parser.parse_packet(&payload);
-                    // process_packet(&mut buffer, &payload);
-                    next_seq += payload.len() as u32;
-                    while future_packets.contains_key(&next_seq) {
-                        let payload = future_packets.remove(&next_seq).unwrap();
-                        println!("found future packet {}", next_seq);
-                        parser.parse_packet(&payload);
-                        next_seq += payload.len() as u32;
-                    }
-                }
-                next_seq = next_seq & 0xffffffff;
+                parse_tcp(tcp);
+            }
+        } else if let Some(v4p) = Ipv4Packet::new(packet.data) {
+            // no eth packet, possibly ip-level routed
+            if let IpNextHeaderProtocols::Tcp = v4p.get_next_level_protocol() {
+                let tcp = TcpPacket::new(v4p.payload()).unwrap();
+                parse_tcp(tcp);
+            } else {
+                eprintln!("not IP!")
             }
         }
     }
